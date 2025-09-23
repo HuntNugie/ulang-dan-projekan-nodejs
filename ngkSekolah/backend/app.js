0a240914b4e7f68f92dbe60d68df87df0a6f18b8
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-23 21:55:12 +0700

    projek/ngkSekolah/tailwindcss + vite untuk halaman front end

--- ngkSekolah/backend/app.js
+++ ngkSekolah/backend/app.js
@@ -1,6 +1,5 @@
 import polka from "polka"
 import axios from "axios"
-import fs from "fs"
 import parseUrl from "url-parse"
 
 
@@ -8,13 +7,15 @@ import parseUrl from "url-parse"
 const app = polka()
 const API = "https://api-sekolah-indonesia.vercel.app/sekolah"
 
+// endpoint pertama
 app.get("/",(req,res)=>{
     res.setHeader("Access-Control-Allow-Origin","*")
     res.setHeader("Content-type","application/json")
     res.end(JSON.stringify({nama:"Nugie kurniawan"}))
 });
 
-app.get("/update/API/perPage",async(req,res)=>{
+// berdasarkan perPage
+app.get("/API/sekolah/perPage",async(req,res)=>{
     try{
         const prs = parseUrl(req.url,true)
         const resultUrl = prs.query
@@ -26,23 +27,26 @@ app.get("/update/API/perPage",async(req,res)=>{
         }
         const response = await axios.get(api)
         const data = response.data
-        fs.writeFileSync("perPage.json",JSON.stringify(data))
+        const result = data.dataSekolah
+        // fs.writeFileSync("perPage.json",JSON.stringify(data))
         res.setHeader("Access-Control-Allow-Origin","*")
         res.setHeader("Control-type","application/json")
-        res.end(JSON.stringify({result:true,message:"Success"}))
+        res.end(JSON.stringify(result))
     }catch(err){
         res.setHeader("Access-Control-Allow-Origin","*")
         res.setHeader("Control-type","application/json")
         res.end(JSON.stringify({result:false,message:err.message}))        
     }
 });
-app.get("/API/perPage",(req,res)=>{
-    // baca file json
-    const data = fs.readFileSync("perPage.json","utf-8")
-    res.setHeader("Access-Control-Allow-Origin","*")
-    res.setHeader("Control-type","application/json")
-    res.end(data)
+
+app.get("/API/sekolah/:nama",(req,res)=>{
+    const {nama} = req.params
+    res.end(nama)
 })
+
+
+
+
 app.listen(3000,err=>{
     if(err)throw err
     console.log("Web berjalan di server http://localhost:3000")
